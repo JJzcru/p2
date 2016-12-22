7b66d67a5c429e04fcddc9fe5034fb3673613c42
Jose J. Cruz <jjzcru@gmail.com>
2016-12-22 17:42:20 -0500

    Refactor naming convention

--- p2.go
+++ p2.go
@@ -2,24 +2,24 @@ package p2
 
 import "unsafe"
 
-//PointerToString transform a pointer to its string value
-func PointerToString(s *string) string {
+//String transform a pointer to its string value
+func String(s *string) string {
 	if s == nil {
 		return ""
 	}
 	return *(*string)(unsafe.Pointer(s))
 }
 
-//PointerToBool transform a pointer to its bool value
-func PointerToBool(b *bool) bool {
+//Bool transform a pointer to its bool value
+func Bool(b *bool) bool {
 	if b == nil {
 		return false
 	}
 	return *(*bool)(unsafe.Pointer(b))
 }
 
-//PointerToInt transform a pointer to its int value
-func PointerToInt(i *int) int {
+//Int transform a pointer to its int value
+func Int(i *int) int {
 	if i == nil {
 		return 0
 	}
